bcc8234d77b74150f44fc5637d40666b0b7e366f
Porramet Taeng-orn <37680321+porrametict@users.noreply.github.com>
2019-02-16 22:08:46 +0700

    init

---
+++ main.js
@@ -0,0 +1,13 @@
+
+function kitkat () {
+    document.getElementById("name").style.display = "block"
+    setTimeout(showTop,5500)
+}
+
+function showTop () {
+    document.getElementById("top_container").style.display = "flex"
+}
+setTimeout (kitkat, 2100);
+
+
+
